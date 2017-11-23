db4b81f778ea4bf4f4a0e67e7b90cfbbb57ac23d
Kevin Etchells <kevetchells@hotmail.com>
2017-11-23 07:23:19 +0000

    Change to page URLs - level1 & level2 pages now separated by "/" instead of "-"

--- public/js/pages.js
+++ public/js/pages.js
@@ -13,7 +13,7 @@
         vm.page.level2 = level2 || "";
 
         if (store) {
-            history.pushState(null, null, window.location.origin + "/" + level1 + (level2 ? "-" + level2 : ""));
+            history.pushState(null, null, window.location.origin + "/" + level1 + (level2 ? "/" + level2 : ""));
         }
         
         // Focus to top - visually and for screen readers
@@ -28,7 +28,7 @@
     // HTML5 History - Load correct page depending on URL
     (function () {
         function setPage(store) {
-            var pages = window.location.pathname.replace("/", "").replace(/%20/g, " ").split("-");
+            var pages = window.location.pathname.replace("/", "").replace(/%20/g, " ").split("/");
             changePage((pages.length && pages[0]) ? pages[0] : "welcome", pages.length >= 2 ? pages[1] : "", store);
         }
         setPage(true);
